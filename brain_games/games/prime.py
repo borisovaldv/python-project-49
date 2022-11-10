d13461e35a231f025980e308a1746ff6c07ca054
borisovaldv <borisovaldv@gmail.com>
2022-11-10 20:09:19 +0300

    Created brain-prime game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,17 @@
+import random
+from brain_games.games.const import RANGE
+
+GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(random_number):
+    for i in range(2, random_number):
+        if (random_number % i) == 0:
+            return 'no'
+    return 'yes'
+
+
+def get_question_and_answer():
+    random_number = random.randint(RANGE[0] + 1, RANGE[1])
+    right_answer = is_prime(random_number)
+    return random_number, right_answer
